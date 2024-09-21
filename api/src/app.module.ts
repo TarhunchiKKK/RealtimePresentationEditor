ba4daa25a4b40fe89f81c9efafef99eb31ab2eb6
konstantin <konstabarilo12@gmail.com>
2024-09-21 12:42:34 +0300

    feat(api): database connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { User } from "./users/entities/user.entity";
 
 @Module({
-    imports: [],
+    imports: [
+        UsersModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: "postgres",
+                host: configService.get("DB_HOST"),
+                port: +configService.get("DB_PORT"),
+                username: configService.get("DB_USER"),
+                password: configService.get("DB_PASSWORD"),
+                database: configService.get("DB_NAME"),
+                synchronize: true,
+                entities: [User],
+            }),
+        }),
+    ],
     controllers: [AppController],
 })
 export class AppModule {}
